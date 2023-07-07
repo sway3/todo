4f3475ef916f84c1304949d8365f3fa7cb0f6330
Seungwoo Kim <svvoo31o@gmail.com>
2023-07-07 17:11:46 +0900

    Feat: default value set to original title when editing todo

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -4,11 +4,16 @@ import InputBox from "./UI/InputBox";
 import Button from "./UI/Button";
 
 const EditTodo = (props) => {
-  const [newTitle, setNewTitle] = useState("");
+  const [newTitle, setNewTitle] = useState(props.title);
+  const [newDate, setNewDate] = useState(props.date);
 
   const newTitleChangeHandler = (event) => {
     console.log(event.target.value);
     setNewTitle(event.target.value);
+  };
+
+  const newDateChangeHandler = (event) => {
+    setNewDate(event.target.value);
   };
 
   const todoUpdateHandler = (event) => {
@@ -17,7 +22,7 @@ const EditTodo = (props) => {
     const updatedTodo = {
       id: props.id,
       title: newTitle,
-      date: props.date,
+      date: newDate,
       isEditing: false,
     };
 
@@ -26,6 +31,7 @@ const EditTodo = (props) => {
     console.log(updatedTodo);
 
     setNewTitle("");
+    setNewDate("");
   };
 
   return (
@@ -33,7 +39,15 @@ const EditTodo = (props) => {
       <form onSubmit={todoUpdateHandler}>
         <InputBox
           type="text"
+          value={newTitle}
           onChange={newTitleChangeHandler}
+          width="300px"
+          height="40px"
+        />
+        <InputBox
+          type="date"
+          value={newDate}
+          onChange={newDateChangeHandler}
           width="300px"
           height="40px"
         />
